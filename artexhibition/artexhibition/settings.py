dad0cae6fe3c0ad450f72368f8b75425c9684ce0
unknown <Narendra Gaikwad>
2026-05-15 22:32:34 +0530

    Connected Neon Postgresql

--- artexhibition/artexhibition/settings.py
+++ artexhibition/artexhibition/settings.py
@@ -101,10 +101,25 @@ WSGI_APPLICATION = 'artexhibition.wsgi.application'
 #     )
 # }
 
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse, parse_qsl
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+tmpPostgres = urlparse(DATABASE_URL)
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': tmpPostgres.port or 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
 
