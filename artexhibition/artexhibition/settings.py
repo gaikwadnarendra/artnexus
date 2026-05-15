56e85bf4e915981583c68c7be1b4052035c85372
unknown <Narendra Gaikwad>
2026-05-15 13:48:03 +0530

    Swithched database

--- artexhibition/artexhibition/settings.py
+++ artexhibition/artexhibition/settings.py
@@ -95,10 +95,17 @@ WSGI_APPLICATION = 'artexhibition.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
